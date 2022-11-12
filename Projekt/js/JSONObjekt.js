87f96fdc4e7f9776a540c8f551103b6694f932ec
Victoria <victoria.kazems@gmail.com>
2022-11-12 14:13:44 +0100

    JS-fixes-and-fixed-contact-form

--- Projekt/js/JSONObjekt.js
+++ Projekt/js/JSONObjekt.js
@@ -23,4 +23,73 @@ function laddaData(){
     laddaEpost.value = JSON.parse(localStorage.getItem('epost'));
     laddaMeddelande.value = JSON.parse(localStorage.getItem('meddelande'));
 }
+
+
+function validateData(){
+
+    let validName = new Boolean(false);
+    let valiPhone = new Boolean(false);
+    let validEmail = new Boolean(false);
+    let validMessage = new Boolean(false);
+
+    
+            if(/^[a-öA-Ö ]+$/.test($(document.getElementById('kontaktNamn')).val()) && $(document.getElementById('kontaktNamn')).val().trim()!=="") {
+                $(document.getElementById('kontaktNamn')).css("border-color", "#7FFF00") && $(".namnKrav").css("color", "#7FFF00") && $("#namnValidering").css("visibility", "hidden");
+                validName = true;
+            }
+            else
+             {
+                $(document.getElementById('kontaktNamn')).css("border-color", "red") && $(".namnKrav").css("color", "red") && $("#namnValidering").css("visibility", "visible");
+                validName = false;
+            }
+    
+
+            if(/^\d*$/.test($(document.getElementById('telefon')).val()) && $(document.getElementById('telefon')).val().trim()!=="") 
+            {             
+                    $(document.getElementById('telefon')).css("border-color", "#7FFF00") && $(".telefonKrav").css("color", "#7FFF00") && $("#telefonValidering").css("visibility", "hidden");
+                    validPhone = true;
+                }
+                else
+                 {
+                    $(document.getElementById('telefon')).css("border-color", "red") && $(".telefonKrav").css("color", "red") && $("#telefonValidering").css("visibility", "visible");
+                    validPhone = false;
+                    }
+    
+                if(/^\w+([-+.'][^\s]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/.test($(document.getElementById('epost')).val()) && $(document.getElementById('epost')).val().trim()!=="") {
+                    $(document.getElementById('epost')).css("border-color", "#7FFF00") && $(".epostKrav").css("color", "#7FFF00") && $("#epostValidering").css("visibility", "hidden");
+                    validEmail = true;
+                }
+                else {
+                    $(document.getElementById('epost')).css("border-color", "red") && $(".epostKrav").css("color", "red") && $("#epostValidering").css("visibility", "visible");
+                    validEmail = false;
+                }
+                if(/^[a-öA-Ö0-9 $-/:-?{-~!"^_`\[\]]{10,100}$/.test($(document.getElementById('meddelande')).val()) && $(document.getElementById('meddelande')).val().trim()!=="") {
+                    $(document.getElementById('meddelande')).css("outline-color", "#7FFF00") && $(".meddelandeKrav").css("color", "#7FFF00") && $("#meddelandeValidering").css("visibility", "hidden");
+                    validMessage = true;
+                }
+                else {
+                    $(document.getElementById('meddelande')).css("outline-color", "red") && $(".meddelandeKrav").css("color", "red") && $("#meddelandeValidering").css("visibility", "visible");
+                    validMessage = false;
+                }
+
+                if (validName == true & validPhone == true && validEmail == true && validMessage && true)
+                {
+                    sendMessage();
+                    location.reload();
+                }
+                else 
+                {
+                    showError();
+                }
+
+}
+
+function sendMessage() {
+    alert("Tack för ditt meddelande! Vi kommer att kontakta dig så snart vi kan.");
+  }
+
+  function showError() {
+    alert("Ditt meddelande kunde inte skickas! Vänligen kontrollera alla fälten.");
+  }
 laddaData();
+
--- Projekt/js/ProjektJQuery.js
+++ Projekt/js/ProjektJQuery.js
@@ -1,74 +1,4 @@
-/* Formulär validering */ 
-$(document).ready(function() {
-    $('#kontaktNamn').keyup(function() {
-        if(/^[a-öA-Ö ]+$/.test($(this).val()) && $(this).val().trim()!=="") {
-            $(this).css("border-color", "#7FFF00") && $(".namnKrav").css("color", "#7FFF00") && $("#namnValidering").css("visibility", "hidden");
-        }
-        else {
-            $(this).css("border-color", "red") && $(".namnKrav").css("color", "red") && $("#namnValidering").css("visibility", "visible");
-        }
-    });
-        $('#telefon').keyup(function() {
-            if(/^\d*$/.test($(this).val()) && $(this).val().trim()!=="") {
-                $(this).css("border-color", "#7FFF00") && $(".telefonKrav").css("color", "#7FFF00") && $("#telefonValidering").css("visibility", "hidden");
-            }
-            else {
-                $(this).css("border-color", "red") && $(".telefonKrav").css("color", "red") && $("#telefonValidering").css("visibility", "visible");
-                }
-    });
-        $('#epost').keyup(function() {
-            if(/^\w+([-+.'][^\s]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/.test($(this).val()) && $(this).val().trim()!=="") {
-                $(this).css("border-color", "#7FFF00") && $(".epostKrav").css("color", "#7FFF00") && $("#epostValidering").css("visibility", "hidden");
-            }
-            else {
-                $(this).css("border-color", "red") && $(".epostKrav").css("color", "red") && $("#epostValidering").css("visibility", "visible");
-            }
-    });
-        $('#meddelande').keyup(function() {
-            if(/^[a-öA-Ö0-9 $-/:-?{-~!"^_`\[\]]{10,100}$/.test($(this).val()) && $(this).val().trim()!=="") {
-                $(this).css("outline-color", "#7FFF00") && $(".meddelandeKrav").css("color", "#7FFF00") && $("#meddelandeValidering").css("visibility", "hidden");
-            }
-            else {
-                $(this).css("outline-color", "red") && $(".meddelandeKrav").css("color", "red") && $("#meddelandeValidering").css("visibility", "visible");
-            }
-    });
 
-    /* Validerar ifall ett JSONOjekt har lästs in */
-    $(window).on("load", function() {
-
-        var namnet = $('#kontaktNamn');
-        if(/^[a-öA-Ö ]+$/.test($(namnet).val()) && $(namnet).val().trim()!=="") {
-            $(namnet).css("border-color", "#7FFF00") && $(".namnKrav").css("color", "#7FFF00") && $("#namnValidering").css("visibility", "hidden");
-        }
-        else {
-            $(namnet).css("border-color", "red") && $(".namnKrav").css("color", "red") && $("#namnValidering").css("visibility", "visible");
-        }
-
-        var numret = $('#telefon');
-        if(/^\d*$/.test($(numret).val()) && $(numret).val().trim()!=="") {
-            $(numret).css("border-color", "#7FFF00") && $(".telefonKrav").css("color", "#7FFF00") && $("#telefonValidering").css("visibility", "hidden");
-        }
-        else {
-            $(numret).css("border-color", "red") && $(".telefonKrav").css("color", "red") && $("#telefonValidering").css("visibility", "visible");
-            }
-
-        var eposten = $('#epost');
-        if(/^\w+([-+.'][^\s]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/.test($(eposten).val()) && $(eposten).val().trim()!=="") {
-            $(eposten).css("border-color", "#7FFF00") && $(".epostKrav").css("color", "#7FFF00") && $("#epostValidering").css("visibility", "hidden");
-        }
-        else {
-            $(eposten).css("border-color", "red") && $(".epostKrav").css("color", "red") && $("#epostValidering").css("visibility", "visible");
-        }
-
-        var meddelandet = $('#meddelande');
-        if(/^[a-öA-Ö0-9 $-/:-?{-~!"^_`\[\]]{10,100}$/.test($(meddelandet).val()) && $(meddelandet).val().trim()!=="") {
-            $(meddelandet).css("outline-color", "#7FFF00") && $(".meddelandeKrav").css("color", "#7FFF00") && $("#meddelandeValidering").css("visibility", "hidden");
-        }
-        else {
-            $(meddelandet).css("outline-color", "red") && $(".meddelandeKrav").css("color", "red") && $("#meddelandeValidering").css("visibility", "visible");
-        }
-
-    });
 
     /* Animering gömma karta */
     $('#hideKarta').click(function() {
@@ -76,7 +6,6 @@ $(document).ready(function() {
         $('#hideKarta').hide(800);
     });
 
-});
 
 
 
